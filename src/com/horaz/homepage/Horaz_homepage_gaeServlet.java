38ef724baeb4916a270f2ef9b94796c8281d4218
Jonas Gehring <g.jjoe64@googlemail.com>
2011-11-30 18:59:21 +0100

    navi logik

--- src/com/horaz/homepage/Horaz_homepage_gaeServlet.java
+++ src/com/horaz/homepage/Horaz_homepage_gaeServlet.java
@@ -16,17 +16,19 @@ public class Horaz_homepage_gaeServlet extends HttpServlet {
 
 		String includeJsp;
 
-		String query = req.getQueryString();
+		String query = req.getRequestURI();
 		if (query == null || query.isEmpty() || query.equals("/")) {
 			// home - intro
 			includeJsp = "/about-intro.jsp";
 		} else {
 			includeJsp = query.replace('/', '-').replace(".html", ".jsp");
+			includeJsp = "/"+includeJsp.substring(1);
 		}
 
+		String includeNav = includeJsp.substring(0, includeJsp.indexOf('-'))+".jsp";
 		// draw JSP
 		req.setAttribute("includeJspContent", "/pages"+includeJsp);
-		req.setAttribute("includeJspNavigation", "/sub-navigation"+includeJsp);
+		req.setAttribute("includeJspNavigation", "/sub-navigation"+includeNav);
 		try {
 			req.getRequestDispatcher("/index.jsp").include(req, resp);
 		} catch (ServletException e) {
--- war/sub-navigation/about.jsp
+++ war/sub-navigation/about.jsp
@@ -1,8 +1,9 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
 <div id="sub-menu-container">
 	<ul id="sub-menu">
 		<li class="pos1"><a class="selected" href="/about/intro.html">Intro</a></li>
 		<li class="pos2"><a href="#">IGNITE! Talk</a></li>
-		<li class="pos3"><a href="#">Google+ Page</a></li>
+		<li class="pos3"><a href="/about/googleplus.html">Google+ Page</a></li>
 	</ul>
 </div>
